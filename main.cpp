6e9dda262e5d1124c7c79baadd4a3ba73333f6a8
AryanBandodkar <aryanbandodkar5@gmail.com>
2024-11-02 12:36:52 +0530

    Displays An image on the screen

--- main.cpp
+++ main.cpp
@@ -1,25 +1,97 @@
 #include<iostream>
 #include<SDL2/SDL.h>
+//Global variables to initialise the height and width of the  window
 
 const int WIDTH = 800, HEIGHT = 600;
+
+SDL_Window * gWindow =NULL;
+//image data type that holds the pixels of an image and renders it
+SDL_Surface*  gScreenSurface = NULL;
+SDL_Surface * gHelloWorld = NULL;
+
+//fn to start i SDL
+bool init()
+{
+    //flag to check if it works or not
+    bool success = true;
+
+    //initialises SDL
+    if(SDL_Init(SDL_INIT_VIDEO)<0)
+    {
+        std::cout<<"SDL could not initialize! SDL_Error: "<<SDL_GetError()<<std::endl;
+        success = false;
+    }
+    else
+    {
+        //create window
+        gWindow = SDL_CreateWindow("SDL Tutorial",SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,WIDTH,HEIGHT,SDL_WINDOW_SHOWN);
+        if(gWindow == NULL)
+        {
+            std::cout<<"Window cant be created "<<SDL_GetError()<<std::endl;
+            success = false;
+        }
+        else
+        {
+            //gets  the surface of the window
+
+            gScreenSurface = SDL_GetWindowSurface( gWindow );
+        }
+        return success;
+    }
+
+}
+
+bool loadMedia()
+{
+    //flag to check if it loads or not
+    bool success = true;
+    gHelloWorld = SDL_LoadBMP("test.bmp");
+    if(gHelloWorld == NULL)
+    {
+        std::cout<<"Unable to load image! SDL Error: "<<SDL_GetError()<<std::endl;
+        success = false;
+    }
+    return success;
+}
+
+void close()
+{
+    //Deallocates stuff nd cleanup
+    SDL_FreeSurface(gHelloWorld);
+    gHelloWorld = NULL;
+
+    //destroy 
+    SDL_DestroyWindow( gWindow);
+    gWindow=NULL;
+    SDL_Quit();
+}
+
+
 int main(int argc,char * argv[])
 {
-    SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window *window =SDL_CreateWindow("Hello world",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,WIDTH,HEIGHT,SDL_WINDOW_ALLOW_HIGHDPI);
-    if(NULL==window)
-        std::cout<<"Error";
-
-    SDL_Event windowEvent;
-
-    while (true)
+    if( !init())
+        std::cout<<"Failed to initialise"<<std::endl;
+    else    
     {
-        if(SDL_PollEvent(&windowEvent));
+        if( !loadMedia())
+            std::cout<<"Failed to load media"<<std::endl;
+        else
         {
-            if(SDL_QUIT ==windowEvent.type)
-                break;
+            SDL_BlitSurface(gHelloWorld,NULL,gScreenSurface,NULL);
+            SDL_UpdateWindowSurface( gWindow );
+            SDL_Event e;
+            bool quit = false;
+            while(!quit)
+            {
+                while(SDL_PollEvent(&e))
+                {
+                    if(e.type == SDL_QUIT)
+                        quit = true;
+                }
+            }  
         }
     }
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return EXIT_SUCCESS;
+    close();
+    return 0;
 }
+    
